6c9aa2b03c2b1b27b2ec89d3166c2abf081769ed
Jakub Wilczyński <j.wilczynski@stud.elka.pw.edu.pl>
2020-12-12 13:07:14 +0100

    all UCL matches added, save_UCL_data_to_DB updated

--- save_UCL_data_to_DB.py
+++ save_UCL_data_to_DB.py
@@ -1,3 +1,60 @@
 # Copyright (c) Jakub Wilczyński 2020
 from os import listdir
 from bs4 import BeautifulSoup
+import sqlite3 as lite
+import sys
+
+
+##### CONNECTING TO DATABASE #####
+file_db = 'Matches_DB_test.db'
+try:
+    if path.exists(file_db):
+        raise Exception("Impossible connecting to database. The database doesn't exist.")
+    conn = lite.connect(file_db)   # database SQLite in the file
+    sql = conn.cursor()
+except Exception as e:
+    print(f"ERROR: {e}")
+    exit(-1)
+
+def find_link_and_id(file):
+    """ This function finds link to webpage with match centre. """
+    with open(f"Matches_Test\\{file}", "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file.read(), 'html.parser')
+        link = soup.find("link", rel="canonical").get("href")
+        idx = link.split("/")[4]
+        return link, idx
+
+def find_teams(file):
+    with open(f"Matches_Test\\{file}", "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file.read(), 'html.parser')
+        teams_names = soup.find("div", id="match-header").find_all("a", class_="team-link")
+        teams_ids = soup.find_all("div", class_="pitch-field")
+        results = {"team1_name": teams_names[0].string, "team2_name" : teams_names[1].string,
+                   "team1_id":teams_ids[0].get("data-team-id"), "team2_id":teams_ids[1].get("data-team-id")}
+        return results
+
+def get_paths(file):
+    return path.abspath(file)
+
+def find_date(file):
+    with open(f"Matches_Test\\{file}", "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+        match_date = soup.find_all("div", class_="info-block cleared")[2].find_all("dd")[1].string[5:]
+        assert len(match_date) == 9
+        day, month, year = match_date.split("-")
+        match_date = datetime.date(int("20"+year), MONTHS[month], int(day))
+        return match_date
+
+"""<div class="info-block cleared"><dl><dt>Kick off:</dt><dd>19:45</dd><dt>Date:</dt><dd>Wed, 15-Sep-10</dd></dl></div>"""
+def find_date(file):
+    with open(f"Matches_Test\\{file}", "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+        date = soup.find_all("div", class_="info-block cleared").find("dd").get(string)[5:]
+        return date
+
+
+preview_files = list(filter(lambda x: 'preview' in x, listdir("Matches_Test")))
+squad_files = list(filter(lambda x: 'squad' in x, listdir("Matches_Test")))
+for file in squad_files:
+    print(find_date(file))
+
--- whoscored_effectiveness.py
+++ whoscored_effectiveness.py
@@ -3,8 +3,10 @@ from os import listdir
 from bs4 import BeautifulSoup
 
 ##### important structures #####
-ALL_SEASONS = ['2010/2011', '2011/2012', '2012/2013', '2013/2014', '2014/2015', '2015/2016', '2016/2017', '2017/2018', '2018/2019', '2019/2020']
-Results = {}  # dictionary included the IDs of players whose performance was correctly predicted {match : [list_of_players]}
+ALL_SEASONS = ['2010/2011', '2011/2012', '2012/2013', '2013/2014', '2014/2015', '2015/2016', '2016/2017', '2017/2018',
+               '2018/2019', '2019/2020']
+results = {}  # dictionary included the IDs of players whose performance was correctly predicted {match : [list_of_players]}
+errors = []  # files with possible errors (in case unsuccessful squad downloading)
 
 ########## functions ##########
 def get_preview_squads(season, match_number):
@@ -13,7 +15,8 @@ def get_preview_squads(season, match_number):
     :param match_number: int, number of match included in the name of file
     :param season: string, name of season for current match
     """
-    with open(f"Matches\\{season.replace('/', '_')}\\Match_{str(match_number)}_preview.html", "r", encoding="utf-8") as file:
+    with open(f"Matches\\{season.replace('/', '_')}\\Match_{str(match_number)}_preview.html", "r",
+              encoding="utf-8") as file:
         soup = BeautifulSoup(file.read(), 'html.parser')
         all_elements = soup.find_all("ul", class_="player")
         all_players = []
@@ -23,12 +26,14 @@ def get_preview_squads(season, match_number):
         team_2_preview = all_players[11:]
         return team_1_preview, team_2_preview
 
+
 def get_lineup_squads(season, match_number):
     """This function gets line-up squad from correct file with match number.
     Returns squads of both teams in tuple of lists (team_1_squad, team_2_squad)\n
     :param match_number: int, number of match included in the name of file
     :param season: string, name of season for current match"""
-    with open(f"Matches\\{season.replace('/', '_')}\\Match_{str(match_number)}_squad.html", "r", encoding="utf-8") as file:
+    with open(f"Matches\\{season.replace('/', '_')}\\Match_{str(match_number)}_squad.html", "r",
+              encoding="utf-8") as file:
         soup = BeautifulSoup(file.read(), 'html.parser')
         pitch_element = soup.find_all("div", class_="pitch-field")
         team_1_squad = []
@@ -40,6 +45,7 @@ def get_lineup_squads(season, match_number):
         for player in team_2_elements:
             team_2_squad.append(player.get('data-player-id'))
         return team_1_squad, team_2_squad
+
 
 def compare_squad(preview, squad):
     """This function compares predicted and starting squad.
@@ -61,50 +67,42 @@ def compare_squad(preview, squad):
                 break
     return team_1_results, team_2_results
 
+
 def check_squads():
-    """This function appends results of squads comparison to the dirctionary Results."""
+    """This function appends results of squads comparison to the dirctionary results."""
     matches_of_season, next_matches = 0, 0
-    errors = []
+    global errors
     for season in ALL_SEASONS:
-    #for season in ['2012/2013']:
-        matches_of_season += len(list(      # number of matches in current directory/season
+        matches_of_season += len(list(  # number of matches in current directory/season
             filter(lambda x: "preview" in x,
                    listdir(f"Matches\\{season.replace('/', '_')}")
                    )))
-        for num in range(next_matches+1, matches_of_season+1):
+        for num in range(next_matches + 1, matches_of_season + 1):
             team_1_results, team_2_results = compare_squad(get_preview_squads(season, num),
                                                            get_lineup_squads(season, num))
             if 0 in (team_1_results, team_2_results):
                 errors.append(num)
-            Results[f"Match_{str(num)}_team1"] = team_1_results
-            Results[f"Match_{str(num)}_team2"] = team_2_results
+            results[f"Match_{str(num)}_team1"] = team_1_results
+            results[f"Match_{str(num)}_team2"] = team_2_results
             print(f"Match {str(num)} checked!")
         next_matches = matches_of_season
         print(f"----- Season {season} checked!")
-        print(f"ERRORS: {errors}")
 
 
 def show_results():
     """This function shows results:
     1. Number of all predictions. 2. Number of correct predictions. 3. Effectiveness of WhoScored."""
     correct_predictions = 0
-    for match in Results.keys():
-        correct_predictions += int(len(Results[match]))
-    all_predictions = (len(Results.keys())) * 11
-    print(Results)
+    for match in results.keys():
+        correct_predictions += int(len(results[match]))
+    all_predictions = (len(results.keys())) * 11
+    print(results)
     print("RESULTS:\nAll predicted performances: " + str(all_predictions))
     print("Correct predicted performances: " + str(correct_predictions))
     print("Result: " + str(correct_predictions / all_predictions))
+    if len(errors) > 0:
+        print(50 * "#" + f"ERRORS in the files: {errors}")
 
 ##### SCRIPT #####
 check_squads()
 show_results()
-
-"""
-RESULTS:
-All predicted performances: 24508
-Correct predicted performances: 20052
-Result: 0.8181818181818182
-"""
-
-
